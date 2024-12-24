6aa3c17be613a551be2720b40afb3e7332d58232
MALTHUS ECHICOUA <113396442+echicouamalthus@users.noreply.github.com>
2024-12-24 23:30:50 +0000

    add cors for resolve cross origin problem🔥

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
 const app = new Hono().basePath("/api");
 
-app.get('/', (c) => {
-  return c.json({ message: "Congrats! You've deployed Hono to Vercel" })
-})
+app.use(
+  "*",
+  cors({
+    origin: "http://localhost:9999/api", // Replace with your frontend's origin
+    allowMethods: ["GET", "POST", "PUT", "DELETE"], // Allow specific methods
+    allowHeaders: ["Content-Type", "Authorization", "Cookie"], // Allow specific headers
+  })
+);
+
+app.get("/", (c) => {
+  return c.json({ message: "Congrats! You've deployed Hono to Vercel" });
+});
 
 export default app;
